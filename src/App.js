f473c221bac7c90c116350cd25963488e91be425
Rupak Kunwar <rupakkunwar11@gmail.com>
2022-09-16 18:46:47 -0500

    Added Search Bar And Heading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './App.css'
 import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
 
 const App = () => {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
 
-  useEffect(() => {
-    getMovieRequest();
-  }, [])
   const getMovieRequest = async () => {
     const url = "http://www.omdbapi.com/?s=star wars&apikey=35a44ca3"
 
@@ -17,8 +17,17 @@ const App = () => {
     setMovies(responseJson.Search);
   }
 
+  useEffect(() => {
+    getMovieRequest();
+  }, [])
+
   return (
     <div className='container-fluid movie-app'>
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <MovieListHeading heading='Movies' />
+        <SearchBox />
+      </div>
+
       <div className='row'>
         <MovieList movies={movies} />
       </div>
