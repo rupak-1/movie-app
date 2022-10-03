b17353019ce3ee423c91dee7cff89113c30e4f21
Rupak Kunwar <rupakkunwar11@gmail.com>
2022-10-03 15:30:18 -0500

    Added Favorites

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 import MovieList from './components/MovieList';
 import SearchBox from './components/SearchBox';
 import MovieListHeading from './components/MovieListHeading';
+import AddFavorite from './components/AddFavorites';
+import RemoveFavorites from './components/RemoveFavorites';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [favorites, setFavorites] = useState([]);
   const [searchValue, setSearchValue] = useState('');
 
   const getMovieRequest = async (searchValue) => {
@@ -23,6 +26,14 @@ const App = () => {
     getMovieRequest(searchValue);
   }, [searchValue]);
 
+  const AddFavoriteMovie = (movie) => {
+    const newFavoriteList = [...favorites, movie]
+    setFavorites(newFavoriteList);
+    console.log(newFavoriteList);
+  }
+
+
+
   return (
     <div className='container-fluid movie-app'>
       <div className='row d-flex align-items-center mt-4 mb-4'>
@@ -31,7 +42,13 @@ const App = () => {
       </div>
 
       <div className='row'>
-        <MovieList movies={movies} />
+        <MovieList movies={movies} handleFavoritesClick={AddFavoriteMovie} favoriteComponent={AddFavorite}/>
+      </div>
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <MovieListHeading heading='Favorites' />
+      </div>
+      <div className='row'>
+        <MovieList movies={favorites} handleFavoritesClick={AddFavoriteMovie} favoriteComponent={RemoveFavorites}/>
       </div>
     </div>
   );
