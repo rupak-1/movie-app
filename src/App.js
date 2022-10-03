8554079d7a5535b9883530594ae67fcbbe99b6b1
Rupak Kunwar <rupakkunwar11@gmail.com>
2022-10-03 17:53:01 -0500

    Cleaned Code

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import AddFavorite from './components/AddFavorites';
 import RemoveFavorites from './components/RemoveFavorites';
 
 const App = () => {
+  //state variables
   const [movies, setMovies] = useState([]);
   const [favorites, setFavorites] = useState([]);
   const [searchValue, setSearchValue] = useState('');
 
+  //function to get movies from the api
   const getMovieRequest = async (searchValue) => {
     const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=35a44ca3`;
 
@@ -22,35 +24,42 @@ const App = () => {
     }
   };
 
+  //useEffect hook to get the movies once search value changes
   useEffect(() => {
     getMovieRequest(searchValue);
   }, [searchValue]);
 
+  //useEffect hook to retrieve favorites list from the local storage
   useEffect(() => {
     const movieFavorites = JSON.parse(
       localStorage.getItem('react-movie-app-favorites')
     );
 
-    setFavorites(movieFavorites);
-  }, [])
+    if (movieFavorites) {
+      setFavorites(movieFavorites);
+    }
+  }, []);
 
+  //function to save the favorites list to local storage
   const saveToLocalStorage = (items) => {
-    localStorage.setItem('react-movie-app-favorites', JSON.stringify(items))
-  }
+    localStorage.setItem('react-movie-app-favorites', JSON.stringify(items));
+  };
 
+  //function to add a movie to favorite list
   const AddFavoriteMovie = (movie) => {
-    const newFavoriteList = [...favorites, movie]
+    const newFavoriteList = [...favorites, movie];
     setFavorites(newFavoriteList);
     saveToLocalStorage(newFavoriteList);
-  }
+  };
 
+  //function to remove a movie from favorite list
   const removeFavoriteMovie = (movie) => {
     const newFavoriteList = favorites.filter(
       (favorite) => favorite.imdbID !== movie.imdbID
     );
     setFavorites(newFavoriteList);
     saveToLocalStorage(newFavoriteList);
-  }
+  };
 
 
   return (
@@ -59,15 +68,20 @@ const App = () => {
         <MovieListHeading heading='Movies' />
         <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
       </div>
-
       <div className='row'>
-        <MovieList movies={movies} handleFavoritesClick={AddFavoriteMovie} favoriteComponent={AddFavorite} />
+        <MovieList movies={movies}
+          handleFavoritesClick={AddFavoriteMovie}
+          favoriteComponent={AddFavorite}
+        />
       </div>
       <div className='row d-flex align-items-center mt-4 mb-4'>
         <MovieListHeading heading='Favorites' />
       </div>
       <div className='row'>
-        <MovieList movies={favorites} handleFavoritesClick={removeFavoriteMovie} favoriteComponent={RemoveFavorites} />
+        <MovieList movies={favorites}
+          handleFavoritesClick={removeFavoriteMovie}
+          favoriteComponent={RemoveFavorites}
+        />
       </div>
     </div>
   );
